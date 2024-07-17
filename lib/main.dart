c4bec742f5e6cc0ed0e385eb408751d5c469085d
thomasthd3000 <116863100+thomasthd3000@users.noreply.github.com>
2024-07-17 14:50:58 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
+// import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // const permission = Permission.notification;
+
+  // if (await permission.isDenied) {
+  //   final result = await permission.request();
+  //   if (result.isGranted) {
+  //     print("true");
+  //   } else if (result.isDenied) {
+  //     print("false");
+  //   } else if (result.isPermanentlyDenied) {
+  //     print("denied");
+  //   }
+  // }
+  // await Permission.notification.isDenied.then((value) {
+  //   print(value);
+  //   if (value) {
+  //     print("Request Permission");
+  //     Permission.notification.request();
+  //   }
+  // });
+
   runApp(const App());
 }
